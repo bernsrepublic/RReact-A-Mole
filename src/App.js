b1d6ff86cd8989bc78cfe6b24248b92cf0d2cebe
bernsrepublic <bernsrepublic@gmail.com>
2023-05-16 19:18:24 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import MoleContainer from "./components/MoleContainer";
+import "./App.css";
+
+function App() {
+  let [score, setScore] = useState(0);
+
+  const createMoleHill = (numHills = 9) => {
+    let hills = [];
+    for (let i = 0; i < numHills; i++) {
+      hills.push(<MoleContainer setScore={setScore} score={score} />);
+    }
+    return <div className="mole-hills">{hills}</div>;
+  };
+
+  return (
+    <div className="App">
+      <h1>{score}</h1>
+      {createMoleHill()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoleContainer.jsx
@@ -0,0 +1,22 @@
+import Mole from "./Mole";
+import Empty from "./Empty";
+import { useState } from "react";
+
+export default function MoleContainer() {
+  const [isShowing, setIsShowing] = useState(false);
+
+  const onMoleClick = () => {
+    if (isShowing) {
+      setIsShowing(false);
+    }
+  };
+  return (
+    <div className="mole-container">
+      {isShowing ? (
+        <Mole setIsShowing={setIsShowing} onMoleClick={onMoleClick} />
+      ) : (
+        <Empty setIsShowing={setIsShowing} />
+      )}
+    </div>
+  );
+}
